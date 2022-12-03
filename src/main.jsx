9485a96da1dd1d8b7a0d5fc21319663699d7e1e8
Gvegas <gvegassam@gmail.com>
2022-12-03 15:49:30 +0300

    remove hollow dependensies

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom/client";
 import Home from "./view/components/pages/Home";
 
 import "./view/assets/styles/scss/main.scss";
-import "macro-css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
